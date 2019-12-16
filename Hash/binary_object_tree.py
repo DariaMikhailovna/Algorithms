125689c4fa5547d25d0075904014f57a91244afc
MargoAddams <0610-1994@mail.ru>
2019-12-17 02:57:42 +0300

    добавила магии

--- Hash/binary_object_tree.py
+++ Hash/binary_object_tree.py
@@ -11,10 +11,10 @@ class Node:
 
 class BinaryTree:
     def __init__(self):
-        self.root: Optional[Node] = None
+        self._root: Optional[Node] = None
 
     def __iter__(self):
-        return self._iter_rec(self.root)
+        return self._iter_rec(self._root)
 
     def _iter_rec(self, curr):
         if curr is None:
@@ -25,8 +25,11 @@ class BinaryTree:
         for key, value in self._iter_rec(curr.right):
             yield key, value
 
+    def __setitem__(self, key, value):
+        self.insert(key, value)
+
     def insert(self, key, value):
-        self.root, ret = self._insert_rec(key, value, self.root)
+        self._root, ret = self._insert_rec(key, value, self._root)
         return ret
 
     def _insert_rec(self, key, value, curr=None):
@@ -42,8 +45,14 @@ class BinaryTree:
             curr.right, ret = self._insert_rec(key, value, curr.right)
             return curr, ret
 
+    def __getitem__(self, key):
+        item = self.search(key)
+        if item is None:
+            raise KeyError()
+        return item.key
+
     def search(self, key):
-        return self._search_rec(key, self.root)
+        return self._search_rec(key, self._root)
 
     def _search_rec(self, key, curr=None):
         if curr is None:
@@ -55,8 +64,12 @@ class BinaryTree:
         if key > curr.key:
             return self._search_rec(key, curr.right)
 
+    def __delitem__(self, key):
+        if not self.remove(key):
+            raise KeyError()
+
     def remove(self, key):
-        self.root, ret = self._remove_rec(key, self.root)
+        self._root, ret = self._remove_rec(key, self._root)
         return ret
 
     def _remove_rec(self, key, curr):
--- Hash/hash_table.py
+++ Hash/hash_table.py
@@ -4,68 +4,63 @@ from copy import deepcopy
 
 class HashTable:
     def __init__(self):
-        self.data = [[] for _ in range(10)]
-        self.size = 0
+        self.__data = [[] for _ in range(1000)]
+        self.__size = 0
 
     def __setitem__(self, key, value):
-        self.size += 1
-        if self.size > len(self.data):
-            self.resize()
-        index = self.get_hash(key)
-        if type(self.data[index]) == list:
+        self.__size += 1
+        if self.__size > len(self.__data):
+            self._resize()
+        index = self._get_hash(key)
+        if type(self.__data[index]) == list:
             exists = False
-            for i, x in enumerate(self.data[index]):
+            for i, x in enumerate(self.__data[index]):
                 if x[0] == key:
                     exists = True
-                    self.data[index][i] = (key, value)
-                    self.size -= 1
+                    self.__data[index][i] = (key, value)
+                    self.__size -= 1
                     break
             if not exists:
-                self.data[index].append((key, value))
-            if len(self.data[index]) >= 0:
-                arr = self.data[index]
+                self.__data[index].append((key, value))
+            if len(self.__data[index]) >= 32:
+                arr = self.__data[index]
                 bst = BinaryTree()
-                for i in range(len(arr)):
-                    bst.insert(*arr[i])
-                self.data[index] = bst
+                for key, value in arr:
+                    bst[key] = value
+                self.__data[index] = bst
         else:
-            if not self.data[index].insert(key, value):
-                self.size -= 1
+            if not self.__data[index].insert(key, value):
+                self.__size -= 1
 
     def __getitem__(self, key):
-        index = self.get_hash(key)
-        if type(self.data[index]) == BinaryTree:
-            item = self.data[index].search(key)
-            if item:
-                return item.value
-            else:
-                raise KeyError()
-        for x in self.data[index]:
+        index = self._get_hash(key)
+        if type(self.__data[index]) == BinaryTree:
+            return self.__data[index][key]
+        for x in self.__data[index]:
             if x[0] == key:
                 return x[1]
         raise KeyError()
 
-    def remove(self, key):
-        index = self.get_hash(key)
-        if type(self.data[index]) == BinaryTree:
-            if not self.data[index].remove(key):
-                raise KeyError()
-            self.size -= 1
+    def __delitem__(self, key):
+        index = self._get_hash(key)
+        if type(self.__data[index]) == BinaryTree:
+            del self.__data[index][key]
+            self.__size -= 1
             return
-        for i in range(len(self.data[index])):
-            if self.data[index][i][0] == key:
-                del self.data[index][i]
-                self.size -= 1
+        for i in range(len(self.__data[index])):
+            if self.__data[index][i][0] == key:
+                del self.__data[index][i]
+                self.__size -= 1
                 return
         raise KeyError()
 
-    def get_hash(self, key):
-        return int(key) % len(self.data)
+    def _get_hash(self, key):
+        return int(key) % len(self.__data)
 
-    def resize(self):
-        arr = deepcopy(self.data)
-        self.data = [[] for _ in range(2 * len(self.data))]
-        self.size = 0
+    def _resize(self):
+        arr = deepcopy(self.__data)
+        self.__data = [[] for _ in range(2 * len(self.__data))]
+        self.__size = 0
         for x in arr:
             for key, value in x:
                 self[key] = value
@@ -80,3 +75,5 @@ if __name__ == '__main__':
         t = h[i]
     for i in range(35):
         print(h[i])
+    for i in range(35):
+        del h[i]
