4281652c7b1ecf7d671ad423bfe941faf70a44c2
MargoAddams <0610-1994@mail.ru>
2020-01-23 19:09:12 +0300

    Доработала алгоритм: вынесла в отдельный класс вспомогательные методы для множестви избавилась от самодельной сортировкм

--- Graphs/kruskal_algorithm.py
+++ Graphs/kruskal_algorithm.py
@@ -4,6 +4,32 @@ class Edge:
         self.y = y
         self.weight = weight
 
+    def __str__(self):
+        return str((self.x, self.y, self.weight))
+
+
+class DSU:
+    def __init__(self, vertex_count):
+        self.pr = [i for i in range(vertex_count)]
+        self.size = [1] * vertex_count
+
+    def unite(self, u, v):
+        u = self.get_root(u)
+        v = self.get_root(v)
+        if u == v:
+            return False
+        if self.size[u] > self.size[v]:
+            u, v = v, u
+        self.pr[u] = v
+        self.size[v] += self.size[u]
+        return True
+
+    def get_root(self, vertex):
+        if self.pr[vertex] == vertex:
+            return vertex
+        self.pr[vertex] = self.get_root(self.pr[vertex])
+        return self.pr[vertex]
+
 
 class Kruskal:
     def __init__(self, adjacency_vector):
@@ -13,56 +39,25 @@ class Kruskal:
                 edge = Edge(i, vertex, weight)
                 self.edges.append(edge)
         self.edges_count = len(self.edges)
-        self.pr = [i for i in range(self.edges_count)]
-
-    def sort(self):
-        new_edges = []
-        mn = (None, None, float('inf'))
-        while len(new_edges) < self.edges_count:
-            j = None
-            for i in range(self.edges_count):
-
-                if self.edges[i].weight < mn[2]:
-                    j = i
-                    mn = (self.edges[i].x, self.edges[i].y, self.edges[i].weight)
-            self.edges[j] = Edge(None, None, float('inf'))
-            new_edges.append(Edge(mn[0], mn[1], mn[2]))
-            mn = (None, None, float('inf'))
-        self.edges = new_edges
+        self.dsu = DSU(len(adjacency_vector))
 
     def run_algorithm(self):
-        self.sort()
+        self.edges.sort(key=lambda edge: edge.weight)
         ostov = []
         for edge in self.edges:
-            if self.get_root(edge.x) != self.get_root(edge.y):
-                self.unite(edge.x, edge.y)
+            if self.dsu.unite(edge.x, edge.y):
                 ostov.append(edge)
         return ostov
-
-    def unite(self, u, v):
-        u = self.get_root(u)
-        v = self.get_root(v)
-        # if u.size > v.size:
-        #     u, v = v, u
-        # self.pr[u] = v
-        # v.size += u.size
-        self.pr[u] = v
-
-    def get_root(self, vertex):
-        if self.pr[vertex] == vertex:
-            return vertex
-        self.pr[vertex] = self.get_root(self.pr[vertex])
-        return self.pr[vertex]
 
 
 def main():
     adjacency_vector = [
-        [(1, 7.), (3, 5.)],
-        [(2, 8.), (3, 9.), (4, 7.)],
-        [(4, 5.)],
-        [(4, 15.), (5, 6.)],
-        [(5, 8.), (6, 9,)],
-        [(6, 11.)]
+        [(1, 7), (2, 9), (5, 14)],
+        [(0, 7), (2, 10), (3, 15)],
+        [(0, 9), (1, 10), (3, 11), (5, 2)],
+        [(1, 15), (2, 11), (4, 6)],
+        [(3, 6), (5, 9)],
+        [(0, 14), (2, 2), (4, 9)]
     ]
     kruskal = Kruskal(adjacency_vector)
     result = kruskal.run_algorithm()
