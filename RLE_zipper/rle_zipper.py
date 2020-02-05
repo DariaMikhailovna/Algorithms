be1184be35b73f68df30dd94750da70717e64df5
MargoAddams <0610-1994@mail.ru>
2020-02-05 07:42:32 +0300

    переделала алгоритм для работы с байтами и добавила работу с файлами

--- RLE_zipper/rle_zipper.py
+++ RLE_zipper/rle_zipper.py
@@ -1,96 +1,117 @@
+import os
 
-def zip_array(arr):
-    res = ''
+
+def number_to_byte(num):
+    assert -128 <= num <= 127
+    return int.to_bytes(num, byteorder='little', signed=True, length=1)
+
+
+def byte_to_number(byte):
+    return int.from_bytes(bytes([byte]), byteorder='little', signed=True)
+
+
+def zip_bytes(arr):
+    assert type(arr) == bytes
+    res = b''
     cnt = 1
     for i in range(len(arr)):
         if i == len(arr) - 1:
-            res += str(cnt) + arr[i]
+            res += number_to_byte(cnt) + arr[i:i + 1]
             break
         if arr[i] == arr[i + 1]:
             cnt += 1
             if int(cnt) == 128:
-                res += str(127) + arr[i]
+                res += number_to_byte(127) + arr[i:i + 1]
                 cnt = 1
         else:
-            res += str(cnt) + arr[i]
+            res += number_to_byte(cnt) + arr[i:i + 1]
             cnt = 1
-    res2 = ''
+    final_res = b''
     i = 0
     cnt = 0
-    s = ''
+    s = b''
     while i < len(res):
         j = i
-        while j < len(res) and res[j].isdigit() and not res[j + 1].isdigit() and (res[j] == '1' or res[j] == '2'):
-            cnt += int(res[j])
-            s += res[j + 1]
-            if res[j] == '2':
-                s += res[j + 1]
+        while j < len(res) and 1 <= byte_to_number(res[j]) <= 2:
+            num = byte_to_number(res[j])
+            cnt += num
+            s += res[j + 1:j + 2] * num
             if cnt >= 128:
-                res2 += '-128'
+                final_res += number_to_byte(-128)
                 cnt -= 128
-                res2 += s[:128]
+                final_res += s[:128]
                 s = s[128:]
             j += 2
         if j == i:
-            res2 += res[i]
-            i += 1
+            final_res += res[i:i + 2]
+            i += 2
         else:
             if cnt > 0:
-                res2 += '-' + str(cnt) + s
+                final_res += number_to_byte(-cnt) + s
             cnt = 0
-            s = ''
+            s = b''
             i = j
-    return res2
+    return final_res
 
 
-def dezip_array(arr):
-    res = ''
+def unzip_bytes(arr):
+    res = b''
     i = 0
     while i < len(arr):
-        coef = ''
-        while arr[i].isdigit():
-            coef += arr[i]
-            i += 1
-        if coef:
-            for j in range(int(coef)):
-                res += arr[i]
-        if arr[i] == '-':
-            i += 1
-            coef = ''
-            while arr[i].isdigit():
-                coef += arr[i]
-                i += 1
-            j = 0
-            while j < int(coef):
-                res += arr[i + j]
-                j += 1
-            i += j
-            continue
+        num = byte_to_number(arr[i])
         i += 1
+        if num > 0:
+            res += arr[i:i + 1] * num
+            i += 1
+        else:
+            num = -num
+            res += arr[i:i + num]
+            i += num
     return res
 
 
-def zip_file(file):
-    pass
+def zip_file(file_in, file_out):
+    with open(file_in, 'rb') as f:
+        data = f.read()
+    data_zipped = zip_bytes(data)
+    with open(file_out, 'wb') as f:
+        f.write(data_zipped)
 
 
-def dezip_file(file):
-    pass
+def unzip_file(file_in, file_out):
+    with open(file_in, 'rb') as f:
+        data_zipped = f.read()
+    data = unzip_bytes(data_zipped)
+    with open(file_out, 'wb') as f:
+        f.write(data)
 
 
 def main():
-    arr = '#########....#.###.#.#.##..#...#########'
-    arr = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    arr = 'abcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjl'
-    arr = 'zip-архив и написать вывод. + 2 байта Вариант А. Алгоритм RLE. 1.'
-    arr = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab'
-    arr = 'abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababab'
-    arr = 'ababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababb'
-    res_zip = zip_array(arr)
-    print(res_zip)
-    res_dezip = dezip_array(res_zip)
-    print(res_dezip)
-    assert arr == res_dezip
+    arrs = [
+        b'#########....#.###.#.#.##..#...#########',
+        b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        b'abcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjlabcdefghjuydlgindfjl',
+        b'ddj3lk45kkkf',
+        b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab',
+        b'abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababab',
+        b'ababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababb',
+    ]
+    for arr in arrs:
+        res_zipped = zip_bytes(arr)
+        print(res_zipped)
+        res_unzipped = unzip_bytes(res_zipped)
+        print(res_unzipped)
+        assert res_unzipped == arr
+
+    # filename = 'test_image.png'
+    filename = 'file.bmp'
+    filename_zipped = 'zipped_' + filename + '.bin'
+    filename_unzipped = 'unzipped_' + filename
+
+    zip_file(filename, filename_zipped)
+    unzip_file(filename_zipped, filename_unzipped)
+    print(os.path.getsize(filename))
+    print(os.path.getsize(filename_zipped))
 
 
 if __name__ == '__main__':
